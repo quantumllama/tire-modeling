ed7cf6701bf518fc8cffec6228a7628e711377b4
Michael <michaeltherobot42@gmail.com>
2017-01-15 23:42:16 -0500

    Added smarter initial guesses in fitting func, moved old fitting function with no Kya to a more aptly named function, fulleqn really is now full including Kya.

--- Pacejka_eqn_noKya.m
+++ Pacejka_eqn_noKya.m
@@ -1,5 +1,5 @@
 % takes coeff vals, SA, FZ, dfz, dpi, IA; returns FY
-function FY = Pacejka_fulleqn(coeffs,ay,FZ,P,IA)
+function FY = Pacejka_eqn_noKya(coeffs,ay,FZ,P,IA)
     % values of Fzo=250lbs and pio=12psi were fit
     
     Fzo = 250 * 0.453592 * 9.806; % nominal FZ
@@ -18,41 +18,34 @@ function FY = Pacejka_fulleqn(coeffs,ay,FZ,P,IA)
     % Cy
     pCy1 = coeffs(6);
     
-    % coeffs that belong to Kya
-    pKy1 = coeffs(7);
-    ppy1 = coeffs(8);
-    pKy3 = coeffs(9);
-    pKy4 = coeffs(10);
-    pKy2 = coeffs(11);
-    pKy5 = coeffs(12);
-    ppy2 = coeffs(13);
-    %
+    Kya  = coeffs(7); % begin with it set as By*Cy*Dy
+    
     % By
-    ey   = coeffs(14);
+    ey   = coeffs(8);
     
     % Ey1
-    pEy1 = coeffs(15);
-    pEy2 = coeffs(16);
-    pEy5 = coeffs(17);
+    pEy1 = coeffs(9);
+    pEy2 = coeffs(10);
+    pEy5 = coeffs(11);
     % Ey2
-    pEy3 = coeffs(18);
-    pEy4 = coeffs(19);
+    pEy3 = coeffs(12);
+    pEy4 = coeffs(13);
     
     % SVyy
-    pVy3 = coeffs(20);
-    pVy4 = coeffs(21);
+    pVy3 = coeffs(14);
+    pVy4 = coeffs(15);
     % SVy
-    pVy1 = coeffs(22);
-    pVy2 = coeffs(23);
+    pVy1 = coeffs(16);
+    pVy2 = coeffs(17);
     
     % Kyyo
-    pKy6 = coeffs(24);
-    pKy7 = coeffs(25);
-    ppy5 = coeffs(26);
+    pKy6 = coeffs(18);
+    pKy7 = coeffs(19);
+    ppy5 = coeffs(20);
     % SHy
-    pHy1 = coeffs(27);
-    pHy2 = coeffs(28);
-    eK   = coeffs(29);
+    pHy1 = coeffs(21);
+    pHy2 = coeffs(22);
+    eK   = coeffs(23);
     
     
     % calculate each major variable
@@ -61,7 +54,6 @@ function FY = Pacejka_fulleqn(coeffs,ay,FZ,P,IA)
     
     Cy = pCy1;
     
-    Kya = pKy1*Fzo*(1 + ppy1*dpi)*(1 - pKy3*abs(IA))*sin(pKy4*atan((FZ/Fzo)/((pKy2 + pKy5*(IA^2))*(1 + ppy2*dpi))));
     By = Kya/(Cy+Dy + ey);
     
 %     Ey = (pEy1 + pEy2*dfz)*(1 + pEy5*IA^2 - sign(ay)*(pEy3 + pEy4*IA));
--- fitPacejka.m
+++ fitPacejka.m
@@ -15,6 +15,7 @@ function fitPacejka()
         
         load(sprintf('%s\\Data\\Sweep %d', tiresfolder, sweepnum));
         clear Pfit
+        
         %% Do some initial fitting with only the main variables that have
         %%% some real world significance
         
@@ -43,18 +44,22 @@ function fitPacejka()
         % SHy
         SHy = 0;
         
-        x0 = [Dy Cy By Ey SVy SHy];
+        x0simple = [Dy Cy By Ey SVy SHy];
         
         % fit data to simple model
         options = optimoptions(@lsqnonlin,'MaxFunctionEvaluations',3000);
-        Pfit_simple = lsqnonlin(@simplePacejka,x0,[],[],options,SA,FY);
-
+        simpleFit = lsqnonlin(@simplePacejkaDiff,x0simple,[],[],options,SA,FY);
         
-        %%  move to fitting the more complex model
+        %%  move to fitting the more complex model without the full Kya
         % find some known initial values
         avgFZ = mean(FZ);
         avgP = mean(P);
         avgIA = mean(IA);
+        
+        Fzo = 250 * 0.453592 * 9.806; % nominal FZ
+        dfz = (avgFZ - Fzo)/Fzo; % normalized FZ
+        pio = 12 * 6.89476; % nominal P
+        dpi = (avgP - pio)/pio; % normalized P
         
         % model we fit to, stored in another function
 % %       Dy = uy*FZ
@@ -67,15 +72,39 @@ function fitPacejka()
         % create array of ones for inital points 
         x0 = ones(23,1);
         
-        % use old fitting data to find an inital value, this is enough to
-        % put us into the area of the global minima
-        x0(7) = Pfit_simple(3)*Pfit_simple(2)*Pfit_simple(1);
+        % use old fitting data to find initial values to try and put the
+        % function in the area of the global minima
+        x0(1) = (simpleFit(1)/avgFZ)^(1/4)/2;
+        x0(2) = (simpleFit(1)/avgFZ)^(1/4)/2/dfz;
+        x0(3) = ((simpleFit(1)/avgFZ)^(1/4)-1)/2/dpi;
+        x0(4) = ((simpleFit(1)/avgFZ)^(1/4)-1)/2/(dpi^2);
+        x0(5) = ((simpleFit(1)/avgFZ)^(1/4)+1)/(avgIA^2);
         
-        % fit data to the complex model
-        lsqfcn = @(x)diffPacejka_all_coeffs(x,SA,FY,avgFZ,avgP,avgIA);
+        x0(6) = simpleFit(2);
+        x0(7) = simpleFit(3)*simpleFit(2)*simpleFit(1);
+        
+        x0(9) = simpleFit(4)^(1/2)/2;
+        x0(10) = simpleFit(4)^(1/2)/2/dfz;
+        x0(11) = (simpleFit(4)^(1/2)-1)/2/(avgIA^2);
+        
+        x0(14) = simpleFit(5)/2/avgFZ/avgIA/2;
+        x0(15) = simpleFit(5)/2/avgFZ/avgIA/2/dfz;
+        x0(16) = simpleFit(5)/2/avgFZ;
+        x0(17) = simpleFit(5)/2/avgFZ/dfz;
+        
+        x0(18) = ((((simpleFit(6)+1)/2)*x0(7)+avgFZ*avgIA*(x0(14)+x0(15)*dfz))/avgIA/avgFZ)^(1/2)/2;
+        x0(19) = ((((simpleFit(6)+1)/2)*x0(7)+avgFZ*avgIA*(x0(14)+x0(15)*dfz))/avgIA/avgFZ)^(1/2)/2/dfz;
+        x0(20) = (((((simpleFit(6)+1)/2)*x0(7)+avgFZ*avgIA*(x0(14)+x0(15)*dfz))/avgIA/avgFZ)^(1/2)-1)/dpi;
+        x0(21) = (simpleFit(6)+1)/2/2;
+        x0(22) = (simpleFit(6)+1)/2/2/dfz;
+        
+        x0 = real(x0);
+        
+        % fit data to the complex model without the full Kya eqn
+        lsqfcn = @(x)fullPacejkaDiff(x,SA,FY,avgFZ,avgP,avgIA);
         options = optimoptions(@lsqnonlin,'MaxFunctionEvaluations',6000);
         Pfit.labels.all = {'pDy1', 'pDy2', 'ppy3', 'ppy4', 'pDy3',...
-            'pCy1', 'Kya',...Kya coeffs omittied for now, 'pKy1', 'ppy1', 'pKy3', 'pKy4', 'pKy2', 'pKy5', 'ppy2',...
+            'pCy1', 'Kya',...
             'ey', 'pEy1', 'pEy2', 'pEy5', 'pEy3', 'pEy4', 'pVy3', 'pVy4',...
             'pVy1', 'pVy2', 'pKy6', 'pKy7', 'ppy5', 'pHy1', 'pHy2', 'eK'};
         Pfit.vals.all = lsqnonlin(lsqfcn,x0,[],[],options);
@@ -83,6 +112,63 @@ function fitPacejka()
         
         Pfit.labels.main = {'Dy' 'Cy' 'By' 'Ey1' 'Ey2' 'SVy' 'SHy'};
         Pfit.vals.main = maincoeffs;
+        
+        %% save and print results, will move to saving specific results later
+%         save(sprintf('%s\\Data\\Sweep %d', tiresfolder, sweepnum), 'Pfit', 'Pcurve' '-append')
+%         save(sprintf('%s\\Data\\aSweep %d', tiresfolder, sweepnum))
+        
+        % set fig size as 1920maincoeffs080
+        f = figure('Position',[0 0 1920 1080]);
+        
+        % plot FZ, SA, FY vs ET
+        figure(f)
+        subplot(3,2,1)
+        plot(ET,FZ)
+        title('FZ')
+        
+        subplot(3,2,3)
+        plot(ET,SA)
+        title('SA')
+        
+        subplot(3,2,5)
+        plot(ET,FY)
+        title('FY')
+        
+        % plot FY vs SA and Pacejka curve
+        % also display avg FZ, P, V, IA
+        subplot(3,2,[2 6])
+        plot(SA,FY,'.r',SA,Pcurve,'b')
+        title(sprintf('Sweep %d: FZ=%0.1f, P=%0.1f, V=%0.1f, IA=%0.1f', sweepnum, mean(FZ), mean(P), mean(V), mean(IA)))
+        
+        % print fig as jpg
+        print(sprintf('%s\\Figures\\Sweep %d', tiresfolder, sweepnum), '-dpng', '-r0')
+        
+        fprintf('Dy = maincoeffs(1)=%f \n', maincoeffs(1))
+        fprintf('Cy = maincoeffs(2)=%f \n', maincoeffs(2))
+        fprintf('By = maincoeffs(3)=%f \n', maincoeffs(3))
+        fprintf('Ey1 = maincoeffs(4)=%f, Ey2 = maincoeffs(5)=%f \n', maincoeffs(4), maincoeffs(5))
+        fprintf('SVy =  maincoeffs(6)=%f \n', maincoeffs(6))
+        fprintf('SHy = maincoeffs(7)=%f \n\n', maincoeffs(7))
+        
+        clear f;
+        
+        %% fit
+        x0 = ones(29,1);
+        x0(1:6) = Pfit.vals.all(1:6);
+        x0(
+        x0(14:29) = Pfit.vals.all(8:23);
+        
+        lsqfcn = @(x)fullPacejkaDiff2(x,SA,FY,avgFZ,avgP,avgIA);
+        options = optimoptions(@lsqnonlin,'MaxFunctionEvaluations',6000);
+        Pfit2.labels.all = {'pDy1', 'pDy2', 'ppy3', 'ppy4', 'pDy3',...
+            'pCy1', 'pKy1', 'ppy1', 'pKy3', 'pKy4', 'pKy2', 'pKy5', 'ppy2',...
+            'ey', 'pEy1', 'pEy2', 'pEy5', 'pEy3', 'pEy4', 'pVy3', 'pVy4',...
+            'pVy1', 'pVy2', 'pKy6', 'pKy7', 'ppy5', 'pHy1', 'pHy2', 'eK'};
+        Pfit2.vals.all = lsqnonlin(lsqfcn,x0,[],[],options);
+        Pcurve = maincoeffs(1)*sin(maincoeffs(2)*atan(maincoeffs(3).*(SA + maincoeffs(7)) - (maincoeffs(4)-sign(SA).*maincoeffs(5)).*(maincoeffs(3).*(SA+maincoeffs(7)) - atan(maincoeffs(3).*(SA+maincoeffs(7)))))) + maincoeffs(6);
+        
+        Pfit2.labels.main = {'Dy' 'Cy' 'By' 'Ey1' 'Ey2' 'SVy' 'SHy'};
+        Pfit2.vals.main = maincoeffs;
         
         %% save and print results, will move to saving specific results later
 %         save(sprintf('%s\\Data\\Sweep %d', tiresfolder, sweepnum), 'Pfit', 'Pcurve' '-append')
@@ -127,7 +213,7 @@ end
 
 % set simple fitting model
 % Fyo = Dy*sin(Cy*arctan(By*ay - Ey(By*ay - arctan(By*ay)))) + SVy
-function diff = simplePacejka(x,ay,FY)
+function diff = simplePacejkaDiff(x,ay,FY)
     % define your normal variables in terms of x(n)
     Dy = x(1);
     Cy = x(2);
@@ -141,7 +227,12 @@ function diff = simplePacejka(x,ay,FY)
 end
 
 % generates diff for lsqnonlin using initial values, SA, FY, FZ, P, and IA
-function diff = diffPacejka_all_coeffs(x0,ay,FY,FZ,P,IA)    
+function diff = fullPacejkaDiff(x0,ay,FY,FZ,P,IA)
+    PacejkaFY = Pacejka_eqn_noKya(x0,ay,FZ,P,IA);
+    
+    diff = PacejkaFY - FY;
+end
+function diff = fullPacejkaDiff2(x0,ay,FY,FZ,P,IA)
     PacejkaFY = Pacejka_fulleqn(x0,ay,FZ,P,IA);
     
     diff = PacejkaFY - FY;
